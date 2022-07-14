e86e996f6ffe37479b6530d941da58a37c817991
ayseates <83542829+ayseates@users.noreply.github.com>
2022-07-14 13:02:37 +0300

    14 Temmuz 2022

---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navi extends Component {
+  render() { //tekrardan çalıştırma yenileme
+    return (
+      <div>
+        <h2>Navi Component</h2>
+      </div>
+    );
+  }
+}
