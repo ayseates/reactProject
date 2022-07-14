2f4427d3de2a9b52f9c44ea695549f6d6da510ec
ayseates <83542829+ayseates@users.noreply.github.com>
2022-07-15 00:11:05 +0300

    reactstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React from "react";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import { Col, Container, Row } from "reactstrap";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList title="Category List" />
-          </Col>
-          <Col xs="9">
-            <ProductList title="Product List" />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((Response) => Response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  render() {
+    let productInfo = { title: "ProductList" };
+    let CategoryInfo = { title: "CategoryList" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={CategoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
